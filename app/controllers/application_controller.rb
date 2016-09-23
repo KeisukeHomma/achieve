e10c3a8e5c460f8a43f7590abc12eb486e8fdcc4
Keisuke Homma <ab13891012@gmail.com>
2016-09-23 07:05:21 +0000

    dive8

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,10 @@ class ApplicationController < ActionController::Base
   # before_actionで下で定義したメソッドを実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-　#変数PERMISSIBLE_ATTRIBUTESに配列[:name]を代入
   PERMISSIBLE_ATTRIBUTES = %i(name)
 
   protected
-　　
-　　#deviseのストロングパラメーターにカラム追加するメソッドを定義
+
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
