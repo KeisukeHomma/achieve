9ab3d4cd4c654b28dc97e4999b8f4321d3deef59
Homma <ab13891012.gmail.com>
2016-09-24 17:29:34 +0900

    server setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://blooming-escarpment-15371.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'blooming-escarpment-15371.herokuapp.com/' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
@@ -89,5 +89,5 @@ Rails.application.configure do
    authentication: :plain,
    enable_starttls_auto: true
   }
-  
+
 end
