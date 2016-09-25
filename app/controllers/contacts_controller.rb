71645be95c1129faae2bded62f241ddf0502efd8
Homma <ab13891012.gmail.com>
2016-09-25 16:46:19 +0900

    dive11

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,23 +6,24 @@ class ContactsController < ApplicationController
       @contact = Contact.new
     end
   end
-  
+
   def create
     @contact = Contact.create(contacts_params)
     if @contact.save
       # "お問い合わせありがとうございました"というメッセージを表示
       redirect_to root_path, notice: "お問い合わせが完了しました！"
+      NoticeMailer.sendmail_contact(@contact).deliver
     else
       # 入力フォームを再描画します。
       render action: 'new'
     end
   end
-  
+
   def confirm
     @contact = Contact.new(contacts_params)
     render :new if @contact.invalid?
   end
-  
+
   private
     def contacts_params
       params.require(:contact).permit(:name, :email, :content)
--- app/mailers/notice_mailer.rb
+++ app/mailers/notice_mailer.rb
@@ -11,4 +11,11 @@ class NoticeMailer < ApplicationMailer
     mail to: "ab13891012@gmail.com",
       subject: '【Achieve】ブログが投稿されました'
   end
+
+  def sendmail_contact(contact)
+    @contact = contact
+
+    mail to: "ab13891012@gmail.com",
+      subject: '【Achieve】お問い合わせが完了しました'
+  end
 end
